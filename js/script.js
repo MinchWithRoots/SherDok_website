d691c3e57c493d1e42ae89cbaae6f018d631bb21
MinchWithRoots <mashedem@gmail.com>
2025-04-15 23:36:30 +0300

    media screen for mobile, tablet
    fixed issues, added overlay for mobile-menu. changed font-sizes

--- js/script.js
+++ js/script.js
@@ -1,7 +1,16 @@
 var wrapperMenu = document.querySelector("#menu");
 var mobileMenu = document.querySelector(".mobile-menu");
+var overlay = document.querySelector(".overlay");
 
 wrapperMenu.addEventListener("click", function () {
   wrapperMenu.classList.toggle("open");
   mobileMenu.classList.toggle("active");
+  overlay.classList.toggle("active");
+});
+
+// закрытие при клике на затемнение
+overlay.addEventListener("click", function () {
+  wrapperMenu.classList.remove("open");
+  mobileMenu.classList.remove("active");
+  overlay.classList.remove("active");
 });
